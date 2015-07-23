44149bb183d0eefbe36cb309c8cd9da7a434ef2e
Junjie (Ryan) Zhang <jzhang8299@gmail.com>
2015-07-23 04:01:03 -0400

    first commit

---
+++ both/router/routes.js
@@ -0,0 +1,14 @@
+Router.route('/', {
+  name: 'home'
+});
+
+Router.route('/dashboard', {
+  name: 'dashboard',
+  controller: 'DashboardController'
+});
+
+
+
+Router.plugin('ensureSignedIn', {
+  only: ['dashboard']
+});
---
+++ client/templates/home/home.js
@@ -0,0 +1,37 @@
+Template.home.rendered = function() {
+
+	 $("#geolocation").click(function (event) {
+        event.preventDefault();
+        $(this).html('');
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var geocoder = new google.maps.Geocoder();
+            var latLng   = new google.maps.LatLng( 
+            position.coords.latitude, position.coords.longitude);
+            geocoder.geocode({
+                'latLng': latLng
+            }, function (results, status) {
+                for (var i = 0; i < results[0].address_components.length; i++) {
+                    var address = results[0].address_components[i];
+                    if (address.types[0] == "postal_code") {
+                        $('#zipcode').html(address.long_name);
+                        $('#location').val(results[0].formatted_address);
+                        $('#showMyLocation').hide();
+
+                    }
+                }
+            });
+        });
+        return false;
+    });
+
+
+
+    $('.carousel').carousel({
+    interval: 2000
+});
+
+     $('.carousel2').carousel({
+     interval: 2000
+});
+
+};
