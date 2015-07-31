d481bbd2acfe1576c2a1dac759c0b7731d7f8afe
Junjie (Ryan) Zhang <jzhang8299@gmail.com>
2015-07-31 16:42:44 -0400

    title

--- both/router/meta.js
+++ both/router/meta.js
@@ -2,8 +2,8 @@ if(Meteor.isClient) {
   Meta.config({
       options: {
         // Meteor.settings[Meteor.settings.environment].public.meta.title
-        title: 'Default Project Title',
-        suffix: 'Project Title'
+        title: 'Foodies',
+        suffix: 'All your local Menu in one place'
       }
   });
 }
